2672cba4dbf74258d210f37cb5a713eb9040db9b
ray <ssbzzkk@gmail.com>
2023-03-17 11:01:24 +0900

    생성자와 tostring 오버라이드

--- ch7/src/Util/Test.java
+++ ch7/src/Util/Test.java
@@ -2,15 +2,26 @@ package Util;
 
 public class Test {
 
-<<<<<<< master
 	private String name;
 	private int no;
 	private String tel;
 	
+	
+	
+	public Test(String name, int no, String tel) {
+		super();
+		this.name = name;
+		this.no = no;
+		this.tel = tel;
+	}
 	public void print() {}
 	public int sum() {
 		return 0;
 	}
-=======
->>>>>>> f34d4f1 인스턴스 변수 생성
+	@Override
+	public String toString() {
+		return "Test [name=" + name + ", no=" + no + ", tel=" + tel + "]";
+	}
+	
+	
 }
